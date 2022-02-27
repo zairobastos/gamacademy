d27207e4895b114b064f049b559fae8299c102a6
zairo bastos <zairo_vianahd@hotmail.com>
2022-02-27 20:52:41 -0300

    Limpeza de arquivos desnecessários

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable linebreak-style */
+import React from "react";
+// eslint-disable-next-line linebreak-style
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+	render(<App />);
+	const linkElement = screen.getByText(/learn react/i);
+	expect(linkElement).toBeInTheDocument();
 });
