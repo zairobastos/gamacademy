e5e8127708060f3d3ab15942eeb0796e435152da
zairo bastos <zairo_vianahd@hotmail.com>
2022-02-27 22:20:50 -0300

    Estilo global inserido

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import React from "react";
 // eslint-disable-next-line linebreak-style
 import ReactDOM from "react-dom";
 import App from "./App";
+import GlobalStyle from "./globalStyle/globalStyle";
 
 ReactDOM.render(
 	<React.StrictMode>
+		<GlobalStyle/>
 		<App />
 	</React.StrictMode>,
 	document.getElementById("root")
